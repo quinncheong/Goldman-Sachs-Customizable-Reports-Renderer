5af3bc8355a25b3f6242326c6c7566fbbde36664
Quinn Cheong <quinncheong@hotmail.com>
2022-02-19 13:30:43 +0800

    Set up frontend with nextjs, tailwind, scss

---
+++ frontend/next.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+/**
+ * @type {import('next').NextConfig}	
+ */
+const nextConfig = {
+	reactStrictMode: true,
+	sassOptions: {
+		includePaths: [path.join(__dirname, "styles")],
+	},
+};
+
+module.exports = nextConfig;
