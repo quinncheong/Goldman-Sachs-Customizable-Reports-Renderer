8a241480a048ca5f101a6c027ff8d517a726b9d5
Joel Lim <joellimjieen@hotmail.com>
2022-02-19 12:44:55 +0800

    first springboot init

---
+++ src/main/java/thefoorbarfighters/gsengage/GsengageApplication.java
@@ -0,0 +1,13 @@
+package thefoorbarfighters.gsengage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsengageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsengageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thefoorbarfighters/gsengage/GsengageApplicationTests.java
@@ -0,0 +1,13 @@
+package thefoorbarfighters.gsengage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsengageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
