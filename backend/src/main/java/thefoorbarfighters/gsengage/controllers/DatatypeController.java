9937dce4adea954112ac51734bf9c9d7d17bcdce
Jonathan <jonathantan1425@gmail.com>
2022-03-05 10:17:17 +0800

    Build post mappings for data processing and report

--- backend/src/main/java/thefoorbarfighters/gsengage/controllers/DatatypeController.java
+++ backend/src/main/java/thefoorbarfighters/gsengage/controllers/DatatypeController.java
@@ -1,10 +1,7 @@
 package thefoorbarfighters.gsengage.controllers;
 
 import net.minidev.json.JSONObject;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import thefoorbarfighters.gsengage.service.DatatypeService;
 
 @RestController
@@ -13,9 +10,19 @@ public class DatatypeController {
     // Send POST request to python data processing API
     // Return API response
 
-    @PostMapping("/analyze")
+    @GetMapping("/health")
+    public String index() {
+        return "Welcome to Backend";
+    }
+
+    @PostMapping(value = "/analyze", headers = "Accept=application/json")
     public JSONObject analyzeDatatype(@RequestBody JSONObject rawData){
         return DatatypeService.getDatatype(rawData);
+    }
+
+    @PostMapping(value = "/report", headers = "Accept=application/json")
+    public JSONObject createData(@RequestBody JSONObject rawData){
+        return DatatypeService.getReport(rawData);
     }
 
 }
