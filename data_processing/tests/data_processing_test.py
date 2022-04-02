515c98a43c223c557b58b0bada7228b0c635f37a
Jonathan <jonathantan1425@gmail.com>
2022-04-02 17:24:44 +0800

    Lint Python files

--- data_processing/tests/data_processing_test.py
+++ data_processing/tests/data_processing_test.py
@@ -1,16 +1,17 @@
 import pytest
 from httpx import AsyncClient
 from fastapi import FastAPI
+
 # from starlette.status import HTTP_404_NOT_FOUND, HTTP_422_UNPROCESSABLE_ENTITY
 
 
 class TestCleaningsRoutes:
     @pytest.mark.asyncio
     async def test_routes_exist(self, app: FastAPI, client: AsyncClient) -> None:
-        res = client.post("/", json={'test':'msg'})
+        res = client.post("/", json={"test": "msg"})
         # assert res.status_code != HTTP_404_NOT_FOUND
         assert res.status_code == 200
-        
+
     # @pytest.mark.asyncio
     # async def test_invalid_input_raises_error(self, app: FastAPI, client: AsyncClient) -> None:
     #     res = await client.post(app.post("/process"), json={})
--- report_processing/create_table.py
+++ report_processing/create_table.py
@@ -1,17 +1,47 @@
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 import pandas as pd
+
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/")
 def test():
-    return 'Connection Successful'
+    return "Connection Successful"
 
-@app.route("/create", methods=['POST'])
+
+@app.route("/create", methods=["POST"])
 def create_table():
-    cols = ['positionDate', 'portfolio', 'instrumentType', 'ISIN', 'ticker', 'contractCode', 'coupon', 'maturityDate', 'currency', 'currentFace', 'originalFace', 'price', 'marketValue']
-    cols_rename = {'positionDate': 'Position Date', 'portfolio': 'Portfolio', 'instrumentType': 'Instrument Type', 'ticker': 'Ticker', 'contractCode': 'Contract Code', 'coupon': 'Coupon', 'maturityDate': 'Maturity', 'currency': 'Currency', 'currentFace': 'Current Face', 'originalFace': 'Original Face', 'price': 'Price', 'marketValue': 'Market Value'}
+    cols = [
+        "positionDate",
+        "portfolio",
+        "instrumentType",
+        "ISIN",
+        "ticker",
+        "contractCode",
+        "coupon",
+        "maturityDate",
+        "currency",
+        "currentFace",
+        "originalFace",
+        "price",
+        "marketValue",
+    ]
+    cols_rename = {
+        "positionDate": "Position Date",
+        "portfolio": "Portfolio",
+        "instrumentType": "Instrument Type",
+        "ticker": "Ticker",
+        "contractCode": "Contract Code",
+        "coupon": "Coupon",
+        "maturityDate": "Maturity",
+        "currency": "Currency",
+        "currentFace": "Current Face",
+        "originalFace": "Original Face",
+        "price": "Price",
+        "marketValue": "Market Value",
+    }
     if request.is_json:
         r = request.get_json(force=True)
         test = pd.DataFrame(r)
@@ -19,15 +49,17 @@ def create_table():
         new.rename(columns=cols_rename, inplace=True)
 
         new = new.round(decimals=2)
-        new['Coupon'] = new['Coupon'].apply(lambda x: '' if x == 0.0 else x)
-        new['Position Date'] = pd.to_datetime(new['Position Date'], format='%Y%m%d').dt.date
-        new['Maturity'] = pd.to_datetime(new['Maturity'], format='%m/%d/%Y').dt.date
-        writer = pd.ExcelWriter('output.xlsx',
-                        engine='xlsxwriter',
-                        date_format='yyyymmdd')
-        new.to_excel(writer, sheet_name='Holdings', index=False)
-        workbook  = writer.book
-        worksheet = writer.sheets['Holdings']
+        new["Coupon"] = new["Coupon"].apply(lambda x: "" if x == 0.0 else x)
+        new["Position Date"] = pd.to_datetime(
+            new["Position Date"], format="%Y%m%d"
+        ).dt.date
+        new["Maturity"] = pd.to_datetime(new["Maturity"], format="%m/%d/%Y").dt.date
+        writer = pd.ExcelWriter(
+            "output.xlsx", engine="xlsxwriter", date_format="yyyymmdd"
+        )
+        new.to_excel(writer, sheet_name="Holdings", index=False)
+        workbook = writer.book
+        worksheet = writer.sheets["Holdings"]
         (max_row, max_col) = new.shape
         worksheet.set_column(0, max_col, 20)
         writer.save()
@@ -37,5 +69,6 @@ def create_table():
     else:
         return jsonify({"code": 400, "message": "Something went wrong."}), 400
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
